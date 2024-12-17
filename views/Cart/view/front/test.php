ffe1ec8f99e66f059d9e4292425c0d7de1adfee6
MeriemA26 <meryam.aski@gmail.com>
2024-12-17 13:11:39 +0100

    Update test.php

--- views/Cart/view/front/test.php
+++ views/Cart/view/front/test.php
@@ -1,11 +1,1 @@
-<?php
 
-include __DIR__. 'C:\xampp\htdocs\project1\view\front/config.php';
-
-try {
-    $db = Database::getConnexion();
-    echo "Database connected successfully!";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-?>
