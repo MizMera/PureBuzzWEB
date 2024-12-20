65078dcb8e368435244e2cbd5c5585a714f44fa2
chahdnejm <chahdnj16@gmail.com>
2024-12-20 02:50:35 +0100

    Update register.php

--- controllers/user/register.php
+++ controllers/user/register.php
@@ -95,8 +95,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $mail->isSMTP();
             $mail->Host = 'sandbox.smtp.mailtrap.io';
             $mail->SMTPAuth = true;
-            $mail->Username = '17009d73e75647'; // Mailtrap Username
-            $mail->Password = '6f3a18807b967a'; // Mailtrap Password
+            $mail->Username = 'd374ff99c94918'; // Mailtrap Username
+            $mail->Password = '8b69d35c07a711'; // Mailtrap Password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 2525;
 
